9fa8ed6da34f139019ec1e40be79ff0a821438db
Orlando Rozas <orlandorozasi@gmail.com>
2024-10-12 01:07:58 -0300

    primer commit spring

---
+++ screenmatch/src/main/java/com/aluracursos/screenmatch/ScreenmatchApplication.java
@@ -0,0 +1,23 @@
+package com.aluracursos.screenmatch;
+
+import com.aluracursos.screenmatch.service.ConsumoApi;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScreenmatchApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScreenmatchApplication.class, args);
+	}
+
+
+	@Override
+	public void run(String... args) throws Exception {
+		var ConsumoApi = new ConsumoApi();
+		//var json = ConsumoApi.obtenerDatos("https://www.omdbapi.com/?t=Game+of+thrones&apikey=5e5aa900");
+		var json = ConsumoApi.obtenerDatos("https://coffee.alexflipnote.dev/random.json");
+		System.out.println(json);
+	}
+}
