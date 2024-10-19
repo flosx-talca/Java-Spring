efee4f828437668ccb66966b8f8819420ee71b47
Orlando Rozas <orlandorozasi@gmail.com>
2024-10-19 02:29:33 -0300

    Desadio Spring 1

---
+++ desafiospring/src/test/java/com/aluracursos/desafiospring/DesafiospringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aluracursos.desafiospring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafiospringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
