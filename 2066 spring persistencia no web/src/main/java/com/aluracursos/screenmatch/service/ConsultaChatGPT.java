3a3924d9ee8a702a23e4198645a99992efcbf6a2
flosx-talca <orlandorozasi@gmail.com>
2024-11-13 12:47:51 -0300

    dejando respaldo

---
+++ 2066 spring persistencia no web/src/main/java/com/aluracursos/screenmatch/service/ConsultaChatGPT.java
@@ -0,0 +1,20 @@
+package com.aluracursos.screenmatch.service;
+
+import com.theokanning.openai.completion.CompletionRequest;
+import com.theokanning.openai.service.OpenAiService;
+
+public class ConsultaChatGPT {
+    public static String obtenerTraduccion(String texto) {
+        OpenAiService service = new OpenAiService("APIKEY");
+
+        CompletionRequest requisicion = CompletionRequest.builder()
+                .model("gpt-4o-mini")
+                .prompt("traduce a español el siguiente texto: " + texto)
+                .maxTokens(1000)
+                .temperature(0.7)
+                .build();
+
+        var respuesta = service.createCompletion(requisicion);
+        return respuesta.getChoices().get(0).getText();
+    }
+}
