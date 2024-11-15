296a87479450f8753d0e89b3396c32d730ab8533
Orlando Rozas <orlandorozasi@gmail.com>
2024-11-15 00:11:47 -0300

    iniciando desafio a contruir coneccion a la API

---
+++ literalura/src/main/java/com/aluracursos/literalura/principal/Principal.java
@@ -0,0 +1,14 @@
+package com.aluracursos.literalura.principal;
+
+public class Principal {
+
+    public Principal(){
+
+    }
+    public void muestraElMenu(){
+        String  menu ="""
+                1 - Hola mundos
+                """;
+        System.out.println(menu);
+    }
+}
---
+++ literalura/src/main/java/com/aluracursos/literalura/repository/LibroRepository.java
@@ -0,0 +1,6 @@
+package com.aluracursos.literalura.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface LibroRepository  {
+}
